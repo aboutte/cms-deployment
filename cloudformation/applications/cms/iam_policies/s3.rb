c9f06dbb6feffe87d7d2ccd9edaa07f81247a3a6
Andy Boutte <andyboutte@mac.com>
2016-06-05 11:36:47 -0700

    ran rubocop on a couple of files

--- cloudformation/applications/cms/iam_policies/s3.rb
+++ cloudformation/applications/cms/iam_policies/s3.rb
@@ -1,17 +1,16 @@
-resource 'S3Access', :Type => 'AWS::IAM::Policy', :Properties => {
-                               :PolicyName => 'S3Access',
-                               :PolicyDocument => {
-                                   :Statement => [
-                                       {
-                                           :Effect => 'Allow',
-                                           :Action => [
-                                               's3:Get*',
-                                               's3:List*',
-                                           ],
-                                           :Resource => 'arn:aws:s3:::rean-us-west-2-aboutte/application/environment/*',
-                                       },
-                                   ],
-                               },
-                               :Roles => [ ref('IAMRole') ],
-                           }
-
+resource 'S3Access', Type: 'AWS::IAM::Policy', Properties: {
+  PolicyName: 'S3Access',
+  PolicyDocument: {
+    Statement: [
+      {
+        Effect: 'Allow',
+        Action: [
+          's3:Get*',
+          's3:List*'
+        ],
+        Resource: 'arn:aws:s3:::rean-us-west-2-aboutte/application/environment/*'
+      }
+    ]
+  },
+  Roles: [ref('IAMRole')]
+}
